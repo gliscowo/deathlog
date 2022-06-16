fa4ff166d927807382a01f64455d9cbc5af3be9c
glisco <glisco03@web.de>
2022-06-17 00:18:54 +0200

    fix NPE cause by empty config file, fixes #11

--- src/main/java/com/glisco/deathlog/client/Config.java
+++ src/main/java/com/glisco/deathlog/client/Config.java
@@ -5,40 +5,50 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Config {
 
     private static Config INSTANCE = new Config();
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final String configFileName = "deathlog.json";
 
     public boolean screenshotsEnabled = false;
 
+    public static void load() {
+        if (!Files.exists(configPath())) {
+            save();
+            return;
+        }
+
+        try (var input = Files.newInputStream(configPath())) {
+            INSTANCE = GSON.fromJson(new InputStreamReader(input, StandardCharsets.UTF_8), Config.class);
+        } catch (IOException e) {
+            BaseDeathLogStorage.LOGGER.warn("Could not load config", e);
+        }
+    }
+
     public static void save() {
-        try (var writer = new FileWriter(FabricLoader.getInstance().getConfigDir().resolve(configFileName).toFile())) {
+        try (var output = Files.newOutputStream(configPath()); var writer = new OutputStreamWriter(output, StandardCharsets.UTF_8)) {
             GSON.toJson(INSTANCE, writer);
         } catch (IOException e) {
             BaseDeathLogStorage.LOGGER.warn("Could not save config", e);
         }
     }
 
-    public static void load() {
-        try (var reader = new FileReader(FabricLoader.getInstance().getConfigDir().resolve(configFileName).toFile())) {
-            INSTANCE = GSON.fromJson(reader, Config.class);
-        } catch (IOException e) {
-            if (e instanceof FileNotFoundException) {
-                save();
-            } else {
-                BaseDeathLogStorage.LOGGER.warn("Could not load config", e);
-            }
+    public static Config instance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Config();
         }
+
+        return INSTANCE;
     }
 
-    public static Config instance() {
-        return INSTANCE;
+    private static Path configPath() {
+        return FabricLoader.getInstance().getConfigDir().resolve("deathlog.json");
     }
 }
