6c06db684ab833f65938a445a42e5aeb291cd973
NikitaCartes <nikich98@yandex.ru>
2023-10-02 05:31:34 +0200

    1.20.2

--- src/main/java/com/glisco/deathlog/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/glisco/deathlog/mixin/ClientPlayNetworkHandlerMixin.java
@@ -4,8 +4,11 @@ import com.glisco.deathlog.client.DeathLogClient;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientCommonNetworkHandler;
+import net.minecraft.client.network.ClientConnectionState;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.util.ScreenshotRecorder;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.s2c.play.DeathMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.text.MutableText;
@@ -19,11 +22,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public class ClientPlayNetworkHandlerMixin {
+public abstract class ClientPlayNetworkHandlerMixin extends ClientCommonNetworkHandler {
 
-    @Shadow
-    @Final
-    private MinecraftClient client;
+    protected ClientPlayNetworkHandlerMixin(MinecraftClient client, ClientConnection connection, ClientConnectionState connectionState) {
+        super(client, connection, connectionState);
+    }
 
     @SuppressWarnings("ConstantConditions")
     @Inject(method = "onDeathMessage", at = @At("HEAD"))
